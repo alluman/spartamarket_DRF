6a20d73ce848422bcc8f233c145c66253ff878ee
alluman <mathghost11@gmail.com>
2024-05-01 07:31:44 +0900

    로그인

--- spartamarket_DRF/accounts/urls.py
+++ spartamarket_DRF/accounts/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name='accounts'
 urlpatterns = [
     path('create/', views.CreateView.as_view(), name='create'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
--- spartamarket_DRF/accounts/views.py
+++ spartamarket_DRF/accounts/views.py
@@ -1,16 +1,31 @@
-from rest_framework import generics
+from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializer
 from .models import User
+from django.contrib.auth import authenticate, login, logout
 
-class CreateView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
 
+class CreateView(APIView):
     def post(self, request):
-        serializer = self.get_serializer(data=request.data)
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
             return Response({"message": "회원가입 성공.", "user_id": user.id})
         else:
             return Response(serializer.errors, status=400)
+        
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return Response({'message': '로그인 확인'}, status=status.HTTP_200_OK)
+        return Response({'message': '잘못된 입력입니다.'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+class LogoutView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response({'message': '로그아웃 되었습니다.'}, status=status.HTTP_200_OK)
