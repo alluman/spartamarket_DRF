b397d6f423a8519ec1eaeacb68afe6c5f05fe6fb
alluman <mathghost11@gmail.com>
2024-05-01 16:59:49 +0900

    mvp

--- spartamarket_DRF/products/views.py
+++ spartamarket_DRF/products/views.py
@@ -1,37 +1,49 @@
-from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ProductSerializer
 from .models import Product
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 
 class ProductList(APIView):
     def get(self, request):
         products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        paginator = Paginator(products, 5)
+        page_number = request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        serializer = ProductSerializer(page, many=True)
+        return Response(serializer.data, status=200)
     
 class CreateView(APIView):
+    permission_classes = [IsAuthenticated]
     def post(self, request):
+        request.data['author'] = request.user.id
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
             product = serializer.save()
-            return Response({"message": "글이 작성되었습니다.", "product.id": product.id})
+            return Response({"message": "청 순 수 연", "productId": product.id}, status=201)
         else:
             return Response(serializer.errors, status=400)
-#유저의 접속여부, 현재 접속중인 유저를 작성자로 등록하는 과정이 없음
 
 class UpdateView(APIView):
+    permission_classes = [IsAuthenticated]
     def put(self, request, productId):
         product = get_object_or_404(Product, pk=productId)
+        if request.user != product.author:
+            return Response({"message": "큐 티 수 연"}, status=403)
         serializer = ProductSerializer(product, data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            product = serializer.save()
+            return Response({"message": "청 순 수 연", "product_id": product.id}, status=200)
+        else:
+            return Response(serializer.errors, status=400)
 
 class DeleteView(APIView):
+    permission_classes = [IsAuthenticated]
     def delete(self, request, productId):
         product = get_object_or_404(Product, pk=productId)
+        if request.user != product.author:
+            return Response({"message": "큐 티 수 연"}, status=403)
         product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({"message": "청 순 수 연"}, status=204)
